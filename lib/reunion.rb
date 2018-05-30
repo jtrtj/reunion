9f46c15133bc8a9ad5fc65cf6939c75fc19f7f87
JohnRoemer <jt.r@live.com>
2018-05-30 10:57:49 -0600

    did not get through iteration 4. having trouble accessing a participants paid amount

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -4,8 +4,8 @@ class Reunion
   attr_reader :location,
               :activities
   def initialize(location)
-    @location   = location
-    @activities = []
+    @location     = location
+    @activities   = []
   end
 
   def add_activity(activity)
@@ -16,5 +16,20 @@ class Reunion
     @activities.map do |activity|
       activity.total_cost
     end.sum
+  end
+
+  def owes_or_owed(participant_name)
+    owed = @activities.map do |activity|
+      if activity.participants.has_key?(participant_name)
+        activity.participant_fair_share
+      end
+    end.compact.sum
+    paid = get_a_participant(participant_name)
+    binding.pry
+    owed - paid
+  end
+
+  def get_a_participant(participant_name)
+    @activities.participants.has_key?(participant_name)
   end
 end
--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -3,6 +3,7 @@ require 'minitest/test'
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/reunion'
+require 'pry'
 
 class ReunionTest < Minitest::Test
   def test_reunion_has_a_location_and_starts_with_no_activities
@@ -47,5 +48,29 @@ class ReunionTest < Minitest::Test
     brewery_tour.add_participant(chris)
 
     assert_equal 184.50, reunion.total_cost
+  end
+
+  def test_reunion_can_created_owed_owes_breakdown
+    reunion = Reunion.new('Ft. Collins, CO')
+    bowling = Activity.new('Bowling', 90.00, 21.00)
+    brewery_tour = Activity.new('Brewery Tour', 0.00, 17.50)
+
+    reunion.add_activity(bowling)
+    reunion.add_activity(brewery_tour)
+
+    jill_attributes = { :name => 'Jill', :amount_paid => 100.50 }
+    jill = Participant.new(jill_attributes)
+    chris_attributes = { :name => 'Chris', :amount_paid => 77.50 }
+    chris = Participant.new(chris_attributes)
+    wesker_attributes = { :name => 'Wesker', :amount_paid => 97.00 }
+    wesker = Participant.new(wesker_attributes)
+
+    bowling.add_participant(jill)
+    bowling.add_participant(chris)
+    brewery_tour.add_participant(wesker)
+    brewery_tour.add_participant(jill)
+    brewery_tour.add_participant(chris)
+
+    assert_equal -79.50, reunion.owes_or_owed('Wesker')
   end
 end
